796b1fed21777b9bb2ea3496fda7eaa614b0801f
SriramK <115445832+SKurnella@users.noreply.github.com>
2024-05-05 15:14:39 +0530

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -1,1 +1,11 @@
+let signInButton = document.getElementById("signInBtn");
+let signUpButton = document.getElementById("signUpBtn");
+let name_field = document.getElementById("name-field");
 
+signInButton.addEventListener("click", () => {
+  name_field.style.maxHeight = "0";
+});
+
+signUpButton.addEventListener("click", () => {
+  name_field.style.maxHeight = "200px";
+});
